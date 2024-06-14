6cbcace66dacb6fbc2eacd09af5611efcc0fb62c
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-06-14 10:51:42 +0900

    3-way string 퀵 소트 알고리즘

--- src/string/introduction/ThreeWayStringQuickSort.java
+++ src/string/introduction/ThreeWayStringQuickSort.java
@@ -1,2 +1,56 @@
-package string.introduction;public class ThreeWayStringQuickSort {
+package string.introduction;
+
+public class ThreeWayStringQuickSort {
+    public static void sort(String[] a) {
+        sort(a, 0, a.length - 1, 0);
+    }
+
+    private static void sort(String[] a, int lo, int hi, int d) {
+        if (hi <= lo) {
+            return;
+        }
+
+        int lt = lo, gt = hi;
+        int v = charAt(a[lo], d);
+        int i = lo + 1;
+
+        while (i <= gt) {
+            int t = charAt(a[i], d);
+            if (t < v) {
+                exch(a, lt++, i++);
+            } else if (t > v) {
+                exch(a, i, gt--);
+            } else {
+                i++;
+            }
+        }
+
+        sort(a, lo, lt-1, d);
+        if (v >= 0) {
+            sort(a, lt, gt, d+1);
+        }
+        sort(a, gt+1, hi, d);
+    }
+
+    private static int charAt(String s, int d) {
+        if (d < s.length()) {
+            return s.charAt(d);
+        } else {
+            return -1;
+        }
+    }
+
+    private static void exch(String[] a, int i, int j) {
+        String temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"she", "sells", "seashells", "by", "the", "sea", "shore", "the", "shells", "she", "sells", "are", "surely", "seashells"};
+        sort(a);
+        for (String s : a) {
+            System.out.println(s);
+        }
+    }
 }
