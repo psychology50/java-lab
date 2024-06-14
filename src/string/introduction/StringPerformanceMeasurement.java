f8fee0cda8762cf57119f375b8dd1794af074fa5
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-06-14 10:51:19 +0900

    더하기 연산 vs StringBuilder

--- src/string/introduction/StringPerformanceMeasurement.java
+++ src/string/introduction/StringPerformanceMeasurement.java
@@ -1,2 +1,25 @@
-package string.introduction;public class StringPerformanceMesurement {
+package string.introduction;
+
+public class StringPerformanceMeasurement {
+    public static void main(String[] args) {
+        // + 연산자 사용 시
+        long startTime = System.currentTimeMillis();
+
+        String str = "";
+        for (int i = 0; i < 10000; i++) {
+            str += "x";
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("String: " + (endTime - startTime) + "ms");
+
+        // StringBuilder 사용 시
+        startTime = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 10000; i++) {
+            sb.append("x");
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("StringBuilder: " + (endTime - startTime) + "ms");
+    }
 }
